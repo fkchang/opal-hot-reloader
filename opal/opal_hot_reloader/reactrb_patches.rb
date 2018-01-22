d51ff00550e3b408765729cb0cd423a63c5b63ad
Forrest Chang <fchang@hedgeye.com>
2018-01-22 09:08:14 -0800

    [fixes #16] incorporate works on opal 0.11 react patch from @sfcgeorge

--- opal/opal_hot_reloader/reactrb_patches.rb
+++ opal/opal_hot_reloader/reactrb_patches.rb
@@ -1,60 +1,31 @@
 # patches to support reloading react.rb
+module ReactrbPatchModules
+  module ReactComponent
+    def add_to_global_component_list(instance)
+      (@global_component_list ||= Set.new).add instance
+    end
+
+    def remove_from_global_component_list(instance)
+      @global_component_list.delete instance
+    end
+
+    def force_update!
+      @global_component_list && @global_component_list.each(&:force_update!)
+    end
+  end
+end
+
 class ReactrbPatches
   # React.rb needs to be patched so the we don't keep adding callbacks
   def self.patch!
-    module ::React
-      module Callbacks
-        module ClassMethods
-          def define_callback(callback_name)
-            attribute_name = "_#{callback_name}_callbacks"
-            class_attribute(attribute_name)
-            self.send("#{attribute_name}=", [])
-            define_singleton_method(callback_name) do |*args, &block|
-              # puts "calling new and improved callbacks"
-              callbacks = []
-              callbacks.concat(args)
-              callbacks.push(block) if block_given?
-              self.send("#{attribute_name}=", callbacks)
-            end
-          end
-        end
-      end
+    ::React::Component.extend ReactrbPatchModules::ReactComponent # works
+
+    ::React::Callbacks.alias_method :original_run_callback, :run_callback # works
+    # Easiest place to hook into all components lifecycles
+    ::React::Callbacks.define_method(:run_callback) do |name, *args| # works
+      React::Component.add_to_global_component_list self if name == :before_mount
+      original_run_callback name, *args
+      React::Component.remove_from_global_component_list self if name == :before_unmount
     end
-
-    module ::React
-      module Callbacks
-
-        alias_method :original_run_callback, :run_callback
-
-        def run_callback(name, *args)
-          # monkey patch run callback because its easiest place to hook
-          # into all components lifecycles.
-          React::Component.add_to_global_component_list self if name == :before_mount
-          original_run_callback name, *args
-          React::Component.remove_from_global_component_list self if name == :before_unmount
-        end
-
-      end
-
-      module Component
-
-        def self.add_to_global_component_list instance
-          # puts "Adding #{instance} to component list"
-          (@global_component_list ||= Set.new).add instance
-        end
-
-        def self.remove_from_global_component_list instance
-          # puts "Removing #{instance} from component list"
-          @global_component_list.delete instance
-        end
-
-        def self.force_update!
-          # puts "Forcing global update"
-          @global_component_list && @global_component_list.each(&:force_update!)
-        end
-
-      end
-    end
-
   end
 end
