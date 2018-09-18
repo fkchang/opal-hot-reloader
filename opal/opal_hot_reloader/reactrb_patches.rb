b85321420ca0fb681f91ca575e5a7a3ace7d4bc2
catmando <mitch@catprint.com>
2018-09-18 19:58:46 -0400

    compatible with latest hyperloop

--- opal/opal_hot_reloader/reactrb_patches.rb
+++ opal/opal_hot_reloader/reactrb_patches.rb
@@ -13,19 +13,61 @@ module ReactrbPatchModules
       @global_component_list && @global_component_list.each(&:force_update!)
     end
   end
+  # patches to handle new react error boundry call back
+  module AddForceUpdate
+    # latest hyperloop already has a force_update! method, but legacy hot
+    # reloader expects it to be defined in React::Component.
+    # Once older versions of Hyperloop are deprecated this can be removed.
+    def force_update!
+      Hyperloop::Component.force_update!
+    end
+  end
+  module AddErrBoundry
+    def self.included(base)
+      base.after_error do |*err|
+        @err = err
+        Hyperloop::Component.force_update!
+      end
+      base.define_method :render do
+        @err ? parse_display_and_clear_error : top_level_render
+      end
+    end
+
+    def parse_display_and_clear_error
+      e = @err[0]
+      component_stack = @err[1]['componentStack'].split("\n ")
+      @err = nil
+      display_error(e, component_stack)
+    end
+
+    def display_error(e, component_stack)
+      DIV do
+        DIV { "Uncaught error: #{e}" }
+        component_stack.each do |line|
+          DIV { line }
+        end
+      end
+    end
+  end
 end
 
 class ReactrbPatches
-  # React.rb needs to be patched so the we don't keep adding callbacks
   def self.patch!
-    ::React::Component.extend ReactrbPatchModules::ReactComponent # works
+    if defined?(::React::TopLevelRailsComponent) && ::React::TopLevelRailsComponent.respond_to?(:after_error)
+       puts "new style"
+      ::React::TopLevelRailsComponent.include ReactrbPatchModules::AddErrBoundry
+      ::React::Component.extend ReactrbPatchModules::AddForceUpdate
+    else
+      puts "old style"
+      ::React::Component.extend ReactrbPatchModules::ReactComponent # works
 
-    ::React::Callbacks.alias_method :original_run_callback, :run_callback # works
-    # Easiest place to hook into all components lifecycles
-    ::React::Callbacks.define_method(:run_callback) do |name, *args| # works
-      React::Component.add_to_global_component_list self if name == :before_mount
-      original_run_callback name, *args
-      React::Component.remove_from_global_component_list self if name == :before_unmount
+      ::React::Callbacks.alias_method :original_run_callback, :run_callback # works
+      # Easiest place to hook into all components lifecycles
+      ::React::Callbacks.define_method(:run_callback) do |name, *args| # works
+        React::Component.add_to_global_component_list self if name == :before_mount
+        original_run_callback name, *args
+        React::Component.remove_from_global_component_list self if name == :before_unmount
+      end
     end
   end
 end
