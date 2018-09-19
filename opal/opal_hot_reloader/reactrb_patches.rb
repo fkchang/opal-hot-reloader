ae67f7733caac83a3f1d847cc67092ecefb56ee9
catmando <mitch@catprint.com>
2018-09-19 00:34:52 -0400

    fixed merge issues

--- opal/opal_hot_reloader/reactrb_patches.rb
+++ opal/opal_hot_reloader/reactrb_patches.rb
@@ -22,35 +22,6 @@ module ReactrbPatchModules
     end
   end
 
-  # patches to handle new react error boundry call back
-  module AddErrBoundry
-    def self.included(base)
-      base.alias_method :pre_hot_loader_render, :render
-      base.after_error do |*err|
-        @err = err
-        force_update!
-      end
-      base.define_method :render do
-        @err ? parse_display_and_clear_error : pre_hot_loader_render
-      end
-    end
-
-    def parse_display_and_clear_error
-      e = @err[0]
-      component_stack = @err[1]['componentStack'].split("\n ")
-      @err = nil
-      display_error(e, component_stack)
-    end
-
-    def display_error(e, component_stack)
-      DIV do
-        DIV { "Uncaught error: #{e}" }
-        component_stack.each do |line|
-          DIV { line }
-        end
-      end
-    end
-  end
   module AddForceUpdate
     # latest hyperloop already has a force_update! method, but legacy hot
     # reloader expects it to be defined in React::Component.
